12cd41dc5d9bf6ec7ab7c6ab75170222e10ffaae
Paulius Dambrauskas <82829703+Paulius101@users.noreply.github.com>
2021-06-05 11:30:14 +0300

    Form uzdavinys su skirtingai inpute type panaudojimais

--- js/main.js
+++ js/main.js
@@ -1,9 +1,16 @@
 const buttonDOM = document.querySelector('button');
-const inputDOM = document.querySelector('input');
+const inputMessageDOM = document.querySelector('#message');
+const inputBgColorDOM = document.querySelector('#bg-color');
+const inputTxtColorDOM = document.querySelector('#text-color');
+
 const spanDOM = document.querySelector('span');
+
+
 
 buttonDOM.addEventListener('click', (event) => {
     event.preventDefault();
-    spanDOM.innerText = inputDOM.value;
-    inputDOM.value = '';
+    spanDOM.style.backgroundColor = inputBgColorDOM.value;
+    spanDOM.style.color = inputTxtColorDOM.value;
+    spanDOM.innerText = inputMessageDOM.value;
+    inputMessageDOM.value = '';
 })
