bc7d9edd1b129b24fca998a11b315ca436c7d279
Paulius Dambrauskas <82829703+Paulius101@users.noreply.github.com>
2021-06-07 11:47:55 +0300

    Uzeigele

--- js/main.js
+++ js/main.js
@@ -1,16 +1,28 @@
 const buttonDOM = document.querySelector('button');
-const inputMessageDOM = document.querySelector('#message');
-const inputBgColorDOM = document.querySelector('#bg-color');
-const inputTxtColorDOM = document.querySelector('#text-color');
-
-const spanDOM = document.querySelector('span');
-
-
+const vardasDOM = document.querySelector('#customer');
+const sriubaDOM = document.querySelector('#sriuba');
+const patiekalasDOM = document.querySelector('#patiekalas');
+const desertasDOM = document.querySelector('#desertas');
+const gerimaiDOM = document.querySelectorAll('input[name="gerimas"]');
+const orderDOM = document.querySelector('.order');
+console.log(orderDOM);
 
 buttonDOM.addEventListener('click', (event) => {
     event.preventDefault();
-    spanDOM.style.backgroundColor = inputBgColorDOM.value;
-    spanDOM.style.color = inputTxtColorDOM.value;
-    spanDOM.innerText = inputMessageDOM.value;
-    inputMessageDOM.value = '';
+
+    function menuChecked(item) {
+        if (item.checked) {
+            return 'nori';
+        }
+        return 'nenori';
+    }
+
+    function gerimasChecked(item) {
+        for (let i = 0; i < 3; i++)
+            if (item[i].checked) {
+                return item[i].value;
+            }
+    }
+
+    orderDOM.innerText = `Uzsakovas vardu ${vardasDOM.value}, ${menuChecked(sriubaDOM)} sriubos, ${menuChecked(patiekalasDOM)} pagrindinio patiekalo, ${menuChecked(desertasDOM)} deserto ir ${gerimasChecked(gerimaiDOM)} yra pasirinktas gerimas.`
 })
