f621dc195b43261089f212ae6ef73c89f6cd048d
Paulius Dambrauskas <82829703+Paulius101@users.noreply.github.com>
2021-06-08 19:11:18 +0300

    krepsinis su efektais commit

--- js/main.js
+++ js/main.js
@@ -1,32 +1,48 @@
-const submitDOM = document.querySelector('button');
-const orderDOM = document.querySelector('.order');
-const nameDOM = document.querySelector('#customer');
-const sriubaDOM = document.querySelector('#sriuba');
-const pagrindinisPatiekalasDOM = document.querySelector('#patiekalas');
-const desertasDOM = document.querySelector('#desertas');
-const allGerimaiDOM = document.querySelectorAll('input[name="gerymas"]');
+const namu1DOM = document.querySelector('button[data-points="1"]');
+const namu2DOM = document.querySelector('button[data-points="2"]');
+const namu3DOM = document.querySelector('button[data-points="3"]');
+const sveciu1DOM = document.querySelector('.pirmas[data-points="1"]');
+const sveciu2DOM = document.querySelector('.antras[data-points="2"]');
+const sveciu3DOM = document.querySelector('.trecias[data-points="3"]');
+const namuDOM = document.querySelector('.rezultatas[data-komanda="namu"]');
+const sveciuDOM = document.querySelector('.rezultatas[data-komanda="sveciu"]');
 
+let rezultatas = 0;
+let rezultatas1 = 0;
 
-function arNoriu(DOM) {
-    return DOM.checked ? 'nori' : 'nenori';
-}
-
-function kurisPazymetas(DOMlist) {
-    for (const itemDOM of DOMlist) {
-        if (itemDOM.checked) {
-            return itemDOM.value;
-        }
-    }
-}
-
-submitDOM.addEventListener('click', (e) => {
+namu1DOM.addEventListener('click', (e) => {
     e.preventDefault();
-    let kreipinys = nameDOM.value === '' ? 'nepateikes vardo' : 'vardu ' + nameDOM.
-        value;
-    let gerimas = kurisPazymetas(allGerimaiDOM) ? `${kurisPazymetas(allGerimaiDOM)
-        }` + 'yra pasirinktas gerimas' : 'nepasirinko gerimo';
-    orderDOM.innerText = `Uzsakovas, ${kreipinys}  ${arNoriu(sriubaDOM)} sriubos, ${arNoriu(pagrindinisPatiekalasDOM)} pagrindinio patiekalo, ${arNoriu(desertasDOM)} deserto ir ${gerimas}.`;
+    rezultatas += 1
+    namuDOM.innerText = rezultatas
+})
+
+namu2DOM.addEventListener('click', (e) => {
+    e.preventDefault();
+    rezultatas += 2
+    namuDOM.innerText = rezultatas
+})
+
+namu3DOM.addEventListener('click', (e) => {
+    e.preventDefault();
+    rezultatas += 3
+    namuDOM.innerText = rezultatas
+})
+
+sveciu1DOM.addEventListener('click', (e) => {
+    e.preventDefault();
+    rezultatas1 += 1
+    sveciuDOM.innerText = rezultatas1
+})
+
+sveciu2DOM.addEventListener('click', (e) => {
+    e.preventDefault();
+    rezultatas1 += 2
+    sveciuDOM.innerText = rezultatas1
 })
 
 
-
+sveciu3DOM.addEventListener('click', (e) => {
+    e.preventDefault();
+    rezultatas1 += 3
+    sveciuDOM.innerText = rezultatas1;
+})
