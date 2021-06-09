c6fec224e937fc4435ea7a5c0ca1fbc125594753
Paulius Dambrauskas <82829703+Paulius101@users.noreply.github.com>
2021-06-09 18:55:38 +0300

    15 uzd completed

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
-function generuotiSocials() {
+function generuotiSocials(selector, array) {
+    const selectorDOM = document.querySelector(selector);
+    if (!Array.isArray(array) || array.length === 0) {
+        console.error('ERROR: nerastas elementas');
+        return false;
+    }
+    let HTML = '';
+    for (let e of array) {
+        HTML += `<i class="fa fa-${e}"></i>`
 
+    }
+    selectorDOM.innerHTML = HTML;
 }
 
-const icons = ['facebook', 'twitter', 'linkedin'];
-
+const icons = ['facebook', 'twitter', 'linkedin', 'pinterest'];
+const icons1 = [];
 generuotiSocials('.socials', icons)
