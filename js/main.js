13306522ce3d81659da08549bdfc5b94537291f3
Paulius Dambrauskas <82829703+Paulius101@users.noreply.github.com>
2021-06-10 14:54:18 +0300

    16 uzduotis in progress

--- js/main.js
+++ js/main.js
@@ -1,17 +1,58 @@
-function generuotiSocials(selector, array) {
-    const selectorDOM = document.querySelector(selector);
-    if (!Array.isArray(array) || array.length === 0) {
-        console.error('ERROR: nerastas elementas');
+function renderNav(selector, nuorodosList) {
+    const DOM = document.querySelector(selector);
+    if (!DOM) {
+        console.error(
+            'ERROR: pagal pateikta selector nepavyko rasti jokio DOM elemento'
+        );
         return false;
     }
-    let HTML = '';
-    for (let e of array) {
-        HTML += `<i class="fa fa-${e}"></i>`
-
+    if (typeof selector !== 'string' || selector === '') {
+        console.error(
+            'ERROR: selector turi buti ne tuscias stringas',
+            selector,
+            nuorodosList
+        );
+        return false;
     }
-    selectorDOM.innerHTML = HTML;
+    if (!Array.isArray(nuorodosList) || nuorodosList.length === 0) {
+        console.log('ERROR: nuorodosList turi buti ne tuscias array');
+        return false;
+    }
+
+    let HTML = '';
+
+
+    for (const nuoroda in nuorodosList) {
+        HTML += `<a href="${nuoroda.href}">${nuoroda.title}</a > `;
+    }
+
+    DOM.innerHTML += `<nav> ${HTML} </nav>`;
 }
 
-const icons = ['facebook', 'twitter', 'linkedin', 'pinterest'];
-const icons1 = [];
-generuotiSocials('.socials', icons)
+const menu = [
+    {
+
+        href: '#',
+        title: 'Home'
+
+    },
+    {
+
+        href: '#',
+        title: 'Services'
+
+    },
+    {
+
+        href: '#',
+        title: 'About us'
+
+    },
+    {
+
+        href: '#',
+        title: 'Contact us'
+
+    }
+];
+renderNav('header', menu);
