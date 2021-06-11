01b4dded669e3a4843da5b1932a6d1a41cf00422
Paulius Dambrauskas <82829703+Paulius101@users.noreply.github.com>
2021-06-11 07:52:58 +0300

    17 uzd. veikia

--- js/main.js
+++ js/main.js
@@ -2,29 +2,19 @@ const submitDOM = document.querySelector('button');
 const selectDOM = document.querySelector('label');
 const optionDOM = document.querySelector('.option');
 
-selectDOM.insertAdjacentHTML('beforeend', `<select name="pets" id="pet-select">
-    <option value="">--Please choose an option--</option>
-    <option value="zuikis">Zuikis</option>
-    <option value="barsukas">Barsukas</option>
-    <option value="lape">Lape</option>
-    <option value="vilkas">Vilkas</option>
-    <option value="sernas">Sernas</option>
-    <option value="stirna">Stirna</option>
-    <option value="vovere">Vovere</option>
-</select>`)
-
-
-function kurisPazymetas(selectDOM) {
-    for (const option of selectDOM) {
-        if (option.selected) {
-            return option.value;
-        }
-    }
+const options = ['zuikis', 'barsukas', 'lape', 'vilkas', 'sernas', 'stirna', 'vovere']
+let forma = '';
+for (const i of options) {
+    forma += `<option value="${i}">${i}</option>`
 }
+
+selectDOM.insertAdjacentHTML('afterend', `<select>${forma}</select>`)
+
+const selectedDOM = document.querySelector('select');
+
+
 submitDOM.addEventListener('click', (e) => {
     e.preventDefault();
-
-    optionDOM.innerText = kurisPazymetas(selectDOM)
-
+    optionDOM.innerText = selectedDOM.value;
 })
 
