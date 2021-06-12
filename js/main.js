14a4f149a27f896333e53bb948f25b5026a055a5
Paulius Dambrauskas <82829703+Paulius101@users.noreply.github.com>
2021-06-12 14:10:30 +0300

    18-b done

--- js/main.js
+++ js/main.js
@@ -1,8 +1,22 @@
-const lentosDydisDOM = document.querySelector('.option')
-const submitDOM = document.querySelector('button')
-const lentosInputDOM = document.querySelector('#board')
+const optionDOM = document.querySelector('.option');
+const buttonDOM = document.querySelector('button');
+const inputDOM = document.getElementById('board');
+const boardDOM = document.querySelector('.board');
 
-submitDOM.addEventListener('click', (e) => {
+buttonDOM.addEventListener('click', (e) => {
     e.preventDefault();
-    lentosDydisDOM.innerText = lentosInputDOM.value + 'x' + lentosInputDOM.value
+    const n = parseInt(inputDOM.value);
+    optionDOM.textContent = `${n}x${n}`;
+    renderBoard(boardDOM, n);
 })
+
+function renderBoard(DOMelement, size) {
+    const rowHeight = 100 / size;
+    let HTML = '';
+
+    for (let i = 0; i < size; i++) {
+        HTML += `<div class="row" style="height: ${rowHeight}%;"></div>`;
+    }
+
+    DOMelement.innerHTML = HTML;
+}
