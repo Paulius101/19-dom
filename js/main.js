76f311a71f51eb694022f8ef072bac02bd689f3e
Paulius Dambrauskas <82829703+Paulius101@users.noreply.github.com>
2021-06-12 12:54:34 +0300

    Lentos dydzio sprendinys

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
+const lentosDydisDOM = document.querySelector('.option')
+const submitDOM = document.querySelector('button')
+const lentosInputDOM = document.querySelector('#board')
 
-
-
-
+submitDOM.addEventListener('click', (e) => {
+    e.preventDefault();
+    lentosDydisDOM.innerText = lentosInputDOM.value + 'x' + lentosInputDOM.value
+})
