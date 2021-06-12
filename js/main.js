5d774b12e763c2b0aab365dfddfa78ccc9d45328
Paulius Dambrauskas <82829703+Paulius101@users.noreply.github.com>
2021-06-12 12:02:25 +0300

    17 su vienu ir keliais selectoriais pilnas spr.

--- js/main.js
+++ js/main.js
@@ -1,20 +1,4 @@
-const submitDOM = document.querySelector('button');
-const selectDOM = document.querySelector('label');
-const optionDOM = document.querySelector('.option');
-
-const options = ['zuikis', 'barsukas', 'lape', 'vilkas', 'sernas', 'stirna', 'vovere']
-let forma = '';
-for (const i of options) {
-    forma += `<option value="${i}">${i}</option>`
-}
-
-selectDOM.insertAdjacentHTML('afterend', `<select>${forma}</select>`)
-
-const selectedDOM = document.querySelector('select');
 
 
-submitDOM.addEventListener('click', (e) => {
-    e.preventDefault();
-    optionDOM.innerText = selectedDOM.value;
-})
+
 
