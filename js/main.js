44f5489b40b54ae3564f4e3601219a0b0d8f1498
Paulius Dambrauskas <82829703+Paulius101@users.noreply.github.com>
2021-06-16 09:36:21 +0300

    20 md isprestas

--- js/main.js
+++ js/main.js
@@ -1,18 +1,15 @@
-function time(nuo, iki, step) {
-
+function tortoGabalas(tortuKiekis, zmoniuKiekis, tortoSvoris) {
+    const vienamTortuiZmoniu = Math.ceil(zmoniuKiekis / tortuKiekis)
+    const svorisVienamZmogui = Math.floor(tortoSvoris * 1000 / vienamTortuiZmoniu);
+    console.log(svorisVienamZmogui);
 }
-
-time(1, 4, 31);
-
+console.log(tortoGabalas(3, 17, 1))
 
 
 
 
+// Jei 3000g ir 17 zmoniu, tai 17 / 3 ~6 ir 1000 / 6 = 166
 
-
-
-
-
-
-
-
+// const vienamTortuiZmoniu = zmoniuKiekis / tortuKiekis
+// const svorisVienamZmogui = tortoSvoris * 1000 / vienamTortuiZmoniu);
+ //166
